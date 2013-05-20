364a254960802db948ee6f4962872046cb54db35
Graham Jenson <grahamjenson@maori.geek.nz>
2013-05-21 08:21:48 +1200

    aha

--- rtest.py
+++ rtest.py
@@ -1,24 +1,27 @@
 from rheapq import heappush
 from rheapq import heappop
 from rheapq import heapreplace
+from solver import *
 
 def main(argv):
-  h = []
-  a = [3, 'write spec']
-  heappush(h, [1, 'write code'])
-  heappush(h, [2, 'release product'])
-  heappush(h, a)
-  heappush(h, [4, 'create tests'])
+  s = Solver(Formula([],{}))
   
-  a[0] = 1
-  print h
-  print 'tt', heappop(h)
-  print h
-  print 'tt', heappop(h)
-  print h
-  print 'tt', heappop(h)
-  print h
+  v1 = Variable('1')
+  v2 = Variable('2')
+  v3 = Variable('3')
+  v4 = Variable('4')
+  v5 = Variable('5')
+  v6 = Variable('6')
 
+  c1 = [v1.pos,v2.pos,v3.pos]
+  c2 = [v1.neg, v2.pos, v5.pos]
+
+  print map(lambda x: str(x), c1)
+  print map(lambda x: str(x), c2)
+
+  a = s.simple_resolution(c1,c2)
+
+  print map(lambda x: str(x), a)
   return 0
 
 def target(*args):
--- solver.py
+++ solver.py
@@ -8,8 +8,9 @@ class Variable:
     self.pos.neg = self.neg
     self.name = name
 
+
 class Literal:
-  def __init__(self,var, pos):
+  def __init__(self, var, pos):
     self.var = var
     self.pos = pos
     self.reason = None
@@ -31,6 +32,7 @@ class Literal:
   def unassign(self):
     assert self.assigned == True
     assert self.neg.assigned == False
+    print 'u', self.__str__()
     self.assigned = False
 
   def is_free(self):
@@ -54,7 +56,8 @@ class Literal:
     while tmp:
       w = tmp.pop()
       if not w.propagate(trail,self):
-        self.watches += tmp 
+        self.watches += tmp
+        print 'l', w.__str__()
         return w
     return None
 
@@ -62,17 +65,67 @@ class Literal:
     prefix = ""
     if not self.pos:
       prefix = "-"
+    tmp = prefix + str(self.var.name)
+    if self.reason:
+      tmp += '[r]'
+    if self.is_falsified():
+      tmp += '[-]'
+    elif self.is_satisfied():
+      tmp +='[+]'
+    else:
+      tmp += '[?]'
+    return tmp 
 
-    return prefix + str(self.var.name)
 
+######################Contraints
+class Constraint:
+  def __str__(self):
+    tmp = "["
+    delim = ''
+    for lit in self.lits:
+      tmp+= delim + lit.__str__()      
+      delim = ' ,'
+    tmp += "]"
+    return tmp
 
-######################CLAUSES
-class Clause:
+  def is_unit_clause(self):
+    return False
+
+class UnitClause(Constraint):
   def __init__(self,lits):
+    assert len(lits) == 1
     self.lits = lits
+    self.lits[0].neg.add_watch(self)
+    raise "asd"
+
+  def propagate(self, trail, p):
+    assert p.neg == self.lits[0]
+    return trail.enqueue(self.lits[0], self)
+  
+  def is_unit_clause(self):
+    return True
+  
+
+class Clause(Constraint):
+  def __init__(self,lits):
+    #sort lits to make free lits at the front
+    nlits = []
+    for l in lits:
+      if l.is_free():
+        nlits = [l] + nlits
+      else:
+        nlits = nlits + [l]
+
+    self.lits = nlits
     #clause [1,2,3,4]
     #watch -1 and -2, if they get assigned we
     #move -1 to the beack and watch
+
+    #should be unit
+    if self.lits[0].is_falsified():
+      #print map(lambda x: x.is_free(), self.lits)
+      assert False
+
     self.lits[0].neg.add_watch(self)
     self.lits[1].neg.add_watch(self)
 
@@ -84,33 +137,39 @@ class Clause:
       self.lits[1] = p.neg
 
     # lits[1] must contain the falsified literal
-    assert self.lits[1] == p.neg
+    if self.lits[1] != p.neg:
+      assert False
 
     # look for new literal to watch: no strategy
     for i, lit in enumerate(self.lits[2:]):
       i = i + 2 # manage the offset
       if not lit.is_falsified():
-        self.lits[1] = self.lits[i];
-        self.lits[i] = p.neg;
-        self.lits[1].neg.add_watch(self);
+        self.lits[1] = self.lits[i]
+        self.lits[i] = p.neg
+        self.lits[1].neg.add_watch(self)
         return True; # not unit yet as 
 
     assert self.lits[1].is_falsified();
     # the clause is now either unit or null
+    if not (self.is_unit() or self.is_null()):
+      print 'not null or unit', self.__str__()
+      assert False
+
     p.add_watch(self)
     return trail.enqueue(self.lits[0],self)
-  
 
-  def __str__(self):
-    tmp = "["
-    delim = ''
-    for lit in self.lits:
-      tmp+= delim + lit.__str__()      
-      delim = ' ,'
-    tmp += "]"
-    return tmp
+  def is_unit(self):
+    free = 0
+    for l in self.lits:
+      if l.is_free() or l.is_satisfied():
+        free += 1
+    return free == 1
 
-
+  def is_null(self):
+    for l in self.lits:
+      if not l.is_falsified():
+        return False
+    return True
 ########################FORMULA
 class Formula:
   def __init__(self,clauses, variables):
@@ -186,6 +245,7 @@ class Trail:
       return True
     
     if p.is_falsified():
+      print 'e falsified',p.__str__()
       return False
     elif p.is_satisfied():
       return True
@@ -196,6 +256,7 @@ class Trail:
       return True
 
   def add_to_order(self,p):
+    p.inc_heur()
     heappush(self._order, p)
 
   def decide(self):
@@ -211,7 +272,6 @@ class Trail:
     for l in lits:
       heappush(self._order, l)
       #add lits back
-
     return lit
 
 #########################Solver
@@ -220,7 +280,11 @@ class Solver:
   def __init__(self,formula):
     self.trail = Trail()
     self.formula = formula
-    for name, v in self.formula.variables.items():
+    self.learnt = []
+    #tis breaking on cnfs/sat/aim-100-3_4-yes1-1.cnf but only with this order
+    #for name, v in self.formula.variables.items():
+    for name in [24, 25, 26, 27, 20, 21, 22, 23, 28, 29, 4, 8, 59, 58, 55, 54, 57, 56, 51, 50, 53, 52, 88, 89, 82, 83, 80, 81, 86, 87, 84, 85, 3, 7, 100, 39, 38, 33, 32, 31, 30, 37, 36, 35, 34, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 2, 6, 99, 98, 91, 90, 93, 92, 95, 94, 97, 96, 11, 10, 13, 12, 15, 14, 17, 16, 19, 18, 48, 49, 46, 47, 44, 45, 42, 43, 40, 41, 1, 5, 9, 77, 76, 75, 74, 73, 72, 71, 70, 79, 78]:
+      v = self.formula.variables[name.__str__()]
       self.trail.add_to_order(v.pos)
       self.trail.add_to_order(v.neg)
 
@@ -242,19 +306,92 @@ class Solver:
     return self.trail
 
   def analyze(self, confl):
-    #TODO backtrack till last decision
-    #change decision, and give this conflict as the reason!
+    print 'analyze', confl.__str__()
+    assert confl.is_null() or confl.is_unit()
     p, reason = self.trail.pop_trail()
-    while reason is not None:
+    nlits = self.simple_resolution(confl.lits, reason.lits)
+    print map(str,nlits)
+
+    while not self.has_free_literal(nlits):
+      if reason:
+        ls = reason.lits
+      else:
+        ls = []
       if self.trail.size() != 0:
         p, reason = self.trail.pop_trail()
       else:
+        print 'ran out', confl.__str__(), reason.__str__(), p.neg.__str__(), self.trail.__str__()
         return None
 
-    p.inc_heur()
     
+    
+    c = create_constr(nlits)
+    #print 't', self.trail.__str__()
+    # #print 'p', p.neg.__str__()
+    # while not self.has_free_literal(nlits):
+    #   #print 'c', map(str, nlits)
+    #   print 't', self.trail.__str__() 
+    #   #print map(lambda x: x.is_free(), nlits)
+    #   p, reason = self.trail.pop_trail()
+    
+    c = self.create_constr(nlits)
+    if c is None:
+      print 'failed', self.trail
+      return None
+
+    self.learnt.append(c)
+
     self.trail.enqueue(p.neg, confl)
+
     return confl
+
+
+  def has_falsidied_literal(self,lits):
+    for l in lits:
+      if l.is_falsified():
+        return True
+    return False
+
+  def has_free_literal(self,lits):
+    for l in lits:
+      if l.is_free():
+        return True
+    return False
+
+  def create_constr(self, lits):
+    l = len(lits)
+    if l == 0:
+      print 'conflict'
+      return None
+    elif l == 1:
+      print 'unit'
+      return UnitClause(lits)
+    else:
+      return Clause(lits)
+
+  def simple_resolution(self,lits1,lits2):
+    nlits = []
+    neg = False
+    for l in lits1:
+      if l in nlits:
+        continue
+      if l.neg in lits2:
+        neg = True
+      else:
+        nlits.append(l)
+    
+    if not neg:
+      assert False
+
+    for l in lits2:
+      if l in nlits:
+        continue
+      if l.neg in lits1:
+        neg = True
+      else:
+        nlits.append(l)
+
+    return nlits
 
   def propagate(self):
     #while there are still un propagated lits
